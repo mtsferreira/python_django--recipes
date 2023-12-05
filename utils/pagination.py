46fabba07ab7b2d2570b49561f9c2d202570ae6f
mtsferreira <mateustutulilu@gmail.com>
2023-12-05 20:46:57 -0300

    commit 6 finished

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,5 +1,12 @@
-from django.core.paginator import Paginator
 import math
+
+from django.core.paginator import Paginator
+
+# python -c
+# "import string as s;from random import SystemRandom as
+# sr;print(''.join(sr().choices(s.ascii_letters +
+# s.punctuation, k=64)))"
+
 
 def make_pagination_range(
     page_range,
@@ -32,20 +39,21 @@ def make_pagination_range(
         'first_page_out_of_range': current_page > middle_range,
         'last_page_out_of_range': stop_range < total_pages,
     }
-    
+
+
 def make_pagination(request, queryset, per_page, qty_pages=4):
     try:
-        current_page = int(request.GET.get('page', 1))   # pegar a query string = ?page=..., se nãotiver nada, pega a página 1
+        current_page = int(request.GET.get('page', 1))
     except ValueError:
         current_page = 1
-        
+
     paginator = Paginator(queryset, per_page)
     page_obj = paginator.get_page(current_page)
-    
+
     pagination_range = make_pagination_range(
         paginator.page_range,
         qty_pages,
         current_page
     )
-    
+
     return page_obj, pagination_range
