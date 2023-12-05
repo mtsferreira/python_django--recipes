7f3f0e2d645b05a79a319c5a69eee352f7a8a6e7
mateusrumao <mateus.rumao@acomsistemas.com.br>
2023-12-05 09:57:42 -0300

    commit 3

--- project/settings.py
+++ project/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
